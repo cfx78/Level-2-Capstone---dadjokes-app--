039b8118ce3d8af25f8bd986f80e729066446f47
cfx78 <104265674+cfx78@users.noreply.github.com>
2022-11-04 23:43:02 -0500

    Working Joke Button

---
+++ app.js
@@ -0,0 +1,22 @@
+let btn = document.getElementById('jokeBtn');
+let joke = document.getElementById('newJoke');
+
+function getJoke() {
+	let myHeaders = new Headers();
+	myHeaders.append('Accept', 'application/json');
+
+	let requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow',
+	};
+
+	fetch('https://icanhazdadjoke.com/', requestOptions)
+		.then((response) => response.json())
+		.then((result) => {
+			joke.innerHTML = result.joke;
+		})
+		.catch((error) => console.log('error', error));
+}
+
+btn.addEventListener('click', getJoke);
